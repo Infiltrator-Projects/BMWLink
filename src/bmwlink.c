30a15afd58e555e47fa5cbcc3ee98ed3f95827d2
Shannon Smith <infiltratr@yandex.com>
2026-09-07 00:49:51 +1000

    Validate the LINK 0.15.8 shared boundary

--- src/bmwlink.c
+++ src/bmwlink.c
@@ -7,7 +7,7 @@
 
 #if defined(__APPLE__) && TARGET_OS_IOS
 #ifndef LINK_SOURCE_REVISION
-#define LINK_SOURCE_REVISION "8e6114498b940c491760875fccc0bede97559e14"
+#define LINK_SOURCE_REVISION "a49e349cb434fad9b191b3a68dbf4cc77b14536c"
 #define BMWLINK_DEFINED_LINK_SOURCE_REVISION 1
 #endif
 #include "link/platform/apple/LinkPortableCore.c"
