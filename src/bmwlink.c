3d54206e00cbf4752d2de2b9f1100b058d6d7af9
Shannon Smith <infiltratr@yandex.com>
2026-09-22 14:06:31 +1000

    Align embedded LINK revision with 0.15.50

--- src/bmwlink.c
+++ src/bmwlink.c
@@ -7,7 +7,7 @@
 
 #if defined(__APPLE__) && TARGET_OS_IOS
 #ifndef LINK_SOURCE_REVISION
-#define LINK_SOURCE_REVISION "0acd4acb0b7ce88ab217d1a1aa1fa943ec8a08dd"
+#define LINK_SOURCE_REVISION "ce1e346d58d019eacb285b43ec793e2a5f52fdd9"
 #define BMWLINK_DEFINED_LINK_SOURCE_REVISION 1
 #endif
 #include "link/platform/apple/LinkPortableCore.c"
